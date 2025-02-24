0fee62784ff6bcec5e9f2d5e27f673f146080e87
Bhavuk Jain <bhavuk_92@yahoo.co.in>
2025-02-24 17:49:59 +0530

    Updates

--- component---src-pages-404-tsx-738f29292ca37d873c3d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[218],{2513:function(e,l,n){n.r(l);var t=n(7294),u=n(8227);l.default=e=>t.createElement(u.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-738f29292ca37d873c3d.js.map
